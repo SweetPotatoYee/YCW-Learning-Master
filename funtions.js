33e3de16157eeb31b6f669160a3557a5283e23f1
SweetPotatoYee <sweetpotatoyee@gmail.com>
2024-10-31 19:41:22 +0800

    Update funtions.js

--- funtions.js
+++ funtions.js
@@ -1,5 +1,5 @@
 // 第一部分 - 自動跳轉
-(function() {
+(function () {
     const currentUrl = window.location.href;
 
     if (currentUrl === "https://sso.ntpc.edu.tw/") {
@@ -14,58 +14,18 @@
 })();
 
 // 第二部分 - alwaysOnFocus
-(function() {
-            unsafeWindow.onblur = null;
-            unsafeWindow.blurred = false;
+(function () {
+    const focusListeners = [];
 
-            unsafeWindow.document.hasFocus = () => true;
-            unsafeWindow.window.onFocus = () => true;
+    const focusListener = function (event) {
+        console.log('Element focused:', event.target);
+    };
 
-            // 阻止 visibility 相關的屬性
-            ['hidden', 'mozHidden', 'msHidden', 'webkitHidden'].forEach(prop_name => {
-                Object.defineProperty(document, prop_name, { value: false });
-            });
+    document.querySelector('input').addEventListener('focus', focusListener);
+    focusListeners.push(focusListener);
 
-            Object.defineProperty(document, 'visibilityState', { get: () => 'visible' });
-            Object.defineProperty(document, 'webkitVisibilityState', { get: () => 'visible' });
+    focusListeners.forEach(listener => {
+        document.querySelector('input').removeEventListener('focus', listener);
+    });
 
-            unsafeWindow.document.onvisibilitychange = undefined;
-
-            // 設定事件攔截
-            const blurWhitelist = [
-                HTMLInputElement,
-                HTMLAnchorElement,
-                HTMLSpanElement,
-                HTMLParagraphElement,
-            ];
-
-            const hoverBlacklist = [
-                HTMLIFrameElement,
-                HTMLHtmlElement,
-                HTMLBodyElement,
-                HTMLHeadElement,
-                HTMLFrameSetElement,
-                HTMLFrameElement
-            ];
-
-            var event_handler = (event) => {
-                if (event.type === 'blur' &&
-                    (blurWhitelist.some(type => event.target instanceof type) ||
-                    event.target.classList?.contains('ql-editor'))) {
-                    return;
-                }
-                if (['mouseleave', 'mouseout'].includes(event.type) &&
-                    !hoverBlacklist.some(type => event.target instanceof type)) {
-                    return;
-                }
-                event.preventDefault();
-                event.stopPropagation();
-                event.stopImmediatePropagation();
-            };
-
-            ['visibilitychange', 'webkitvisibilitychange', 'blur', 'hasFocus', 'mouseleave', 'mouseout', 
-             'mozvisibilitychange', 'msvisibilitychange'].forEach(event_name => {
-                window.addEventListener(event_name, event_handler, true);
-                document.addEventListener(event_name, event_handler, true);
-            });
-        })();
+})();
