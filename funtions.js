87ba896dbd56e3c957360be1a2e7459021b807d9
SweetPotatoYee <sweetpotatoyee@gmail.com>
2024-10-31 19:46:35 +0800

    Update funtions.js

--- funtions.js
+++ funtions.js
@@ -3,29 +3,24 @@
     const currentUrl = window.location.href;
 
     if (currentUrl === "https://sso.ntpc.edu.tw/") {
-        setTimeout(() => {
             window.location.href = "https://pts.ntpc.edu.tw/oauth/ntpc_entry_point";
-        }, 1000);
     } else if (currentUrl.startsWith("https://pts.ntpc.edu.tw/#")) {
-        setTimeout(() => {
             window.location.href = "https://pts.ntpc.edu.tw/private/service/goto/service/100/100";
-        }, 1000);
     }
 })();
 
 // 第二部分 - alwaysOnFocus
 (function () {
-    const focusListeners = [];
-
-    const focusListener = function (event) {
-        console.log('Element focused:', event.target);
-    };
-
-    document.querySelector('input').addEventListener('focus', focusListener);
-    focusListeners.push(focusListener);
-
-    focusListeners.forEach(listener => {
-        document.querySelector('input').removeEventListener('focus', listener);
-    });
-
+    const currentUrl = window.location.href;
+    if (currentUrl.startsWith("https://adl.edu.tw/modules_new.php")) {
+        const focusListeners = [];
+        const focusListener = function (event) {
+            console.log('Element focused:', event.target);
+        };
+        document.querySelector('input').addEventListener('focus', focusListener);
+        focusListeners.push(focusListener);
+        focusListeners.forEach(listener => {
+            document.querySelector('input').removeEventListener('focus', listener);
+        });
+    }
 })();
