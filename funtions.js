470abf17dfbe8899ca878a5c3ca752cd43ce30ed
SweetPotatoYee <sweetpotatoyee@gmail.com>
2024-10-31 16:20:03 +0800

    Update funtions.js

--- funtions.js
+++ funtions.js
@@ -15,57 +15,57 @@
 
 // 第二部分 - alwaysOnFocus
 (function() {
-    unsafeWindow.onblur = null;
-    unsafeWindow.blurred = false;
+            unsafeWindow.onblur = null;
+            unsafeWindow.blurred = false;
 
-    unsafeWindow.document.hasFocus = () => true;
-    unsafeWindow.window.onFocus = () => true;
+            unsafeWindow.document.hasFocus = () => true;
+            unsafeWindow.window.onFocus = () => true;
 
-    // 阻止 visibility 相關的屬性
-    ["hidden", "mozHidden", "msHidden", "webkitHidden"].forEach(prop_name => {
-        Object.defineProperty(document, prop_name, { value: false });
-    });
+            // 阻止 visibility 相關的屬性
+            ['hidden', 'mozHidden', 'msHidden', 'webkitHidden'].forEach(prop_name => {
+                Object.defineProperty(document, prop_name, { value: false });
+            });
 
-    Object.defineProperty(document, "visibilityState", { get: () => "visible" });
-    Object.defineProperty(document, "webkitVisibilityState", { get: () => "visible" });
+            Object.defineProperty(document, 'visibilityState', { get: () => 'visible' });
+            Object.defineProperty(document, 'webkitVisibilityState', { get: () => 'visible' });
 
-    unsafeWindow.document.onvisibilitychange = undefined;
+            unsafeWindow.document.onvisibilitychange = undefined;
 
-    // 設定事件攔截
-    const blurWhitelist = [
-        HTMLInputElement,
-        HTMLAnchorElement,
-        HTMLSpanElement,
-        HTMLParagraphElement,
-    ];
+            // 設定事件攔截
+            const blurWhitelist = [
+                HTMLInputElement,
+                HTMLAnchorElement,
+                HTMLSpanElement,
+                HTMLParagraphElement,
+            ];
 
-    const hoverBlacklist = [
-        HTMLIFrameElement,
-        HTMLHtmlElement,
-        HTMLBodyElement,
-        HTMLHeadElement,
-        HTMLFrameSetElement,
-        HTMLFrameElement
-    ];
+            const hoverBlacklist = [
+                HTMLIFrameElement,
+                HTMLHtmlElement,
+                HTMLBodyElement,
+                HTMLHeadElement,
+                HTMLFrameSetElement,
+                HTMLFrameElement
+            ];
 
-    var event_handler = (event) => {
-        if (event.type === 'blur' &&
-            ((blurWhitelist.some(type => event.target instanceof type) ||
-                event.target.classList?.contains('ql-editor')))) {
-            return;
-        }
-        if (['mouseleave', 'mouseout'].includes(event.type) &&
-            !hoverBlacklist.some(type => event.target instanceof type)) {
-            return;
-        }
-        event.preventDefault();
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-    };
+            var event_handler = (event) => {
+                if (event.type === 'blur' &&
+                    (blurWhitelist.some(type => event.target instanceof type) ||
+                    event.target.classList?.contains('ql-editor'))) {
+                    return;
+                }
+                if (['mouseleave', 'mouseout'].includes(event.type) &&
+                    !hoverBlacklist.some(type => event.target instanceof type)) {
+                    return;
+                }
+                event.preventDefault();
+                event.stopPropagation();
+                event.stopImmediatePropagation();
+            };
 
-    ["visibilitychange", "webkitvisibilitychange", "blur", "hasFocus", "mouseleave", "mouseout", 
-     "mozvisibilitychange", "msvisibilitychange"].forEach(event_name => {
-        window.addEventListener(event_name, event_handler, true);
-        document.addEventListener(event_name, event_handler, true);
-    });
-})();
+            ['visibilitychange', 'webkitvisibilitychange', 'blur', 'hasFocus', 'mouseleave', 'mouseout', 
+             'mozvisibilitychange', 'msvisibilitychange'].forEach(event_name => {
+                window.addEventListener(event_name, event_handler, true);
+                document.addEventListener(event_name, event_handler, true);
+            });
+        })();
