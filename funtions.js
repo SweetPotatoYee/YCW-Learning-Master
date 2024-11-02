4560c337a5744e346985dc13711039af221202f9
SweetPotatoYee <sweetpotatoyee@gmail.com>
2024-11-02 15:19:43 +0800

    Update funtions.js

--- funtions.js
+++ funtions.js
@@ -1,16 +1,12 @@
-console.log("YCW Learning Assistant Started")
+console.log("YCW Learning Assistant Started");
 // 第一部分 - 自動跳轉
 (function () {
-    console.log("1")
     const currentUrl = window.location.href;
     if (currentUrl === "https://sso.ntpc.edu.tw/") {
-        console.log("go")
         window.location.href = "https://pts.ntpc.edu.tw/oauth/ntpc_entry_point";
     } else if (currentUrl.startsWith("https://pts.ntpc.edu.tw/#")) {
-        console.log("go")
         window.location.href = "https://pts.ntpc.edu.tw/private/service/goto/service/100/100";
     }
-    console.log("nogo")
 })();
 
 // 第二部分 - alwaysOnFocus
