68d6c5963347cb3a7c2ce2be09325b64c46ce930
SweetPotatoYee <sweetpotatoyee@gmail.com>
2024-11-09 11:48:20 +0800

    update

---
+++ scripts/funtions.js
@@ -0,0 +1,19 @@
+console.log("YCW Learning Assistant Started");
+// 第一部分 - 自動跳轉
+(function () {
+    const currentUrl = window.location.href;
+    if (currentUrl === "https://sso.ntpc.edu.tw/") {
+        window.location.href = "https://pts.ntpc.edu.tw/oauth/ntpc_entry_point";
+    } else if (currentUrl.startsWith("https://pts.ntpc.edu.tw/#")) {
+        window.location.href = "https://pts.ntpc.edu.tw/private/service/goto/service/100/100";
+    }
+})();
+
+// 第二部分 - alwaysOnFocus
+(function () {
+    const currentUrl = window.location.href;
+    if (currentUrl.startsWith("https://adl.edu.tw/modules_new.php")) {
+        $(window).off('blur');
+        bIsBlur = false;
+    }
+})();
